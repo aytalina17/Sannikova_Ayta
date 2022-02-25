7e4a9248ebc5ad6b6845645b1b037ca225970513
Aytalina Sannikova <aytalina170303@gmail.com>
2022-02-25 08:49:05 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+import java.util.Scanner;
+
+public class Homeworks {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int min = 0;
+        int max = 2_147_483_647;
+        int midRange = (min + max) / 2;
+
+        String strInput;
+        String equal = "=";
+        String more = ">";
+        String less = "<";
+
+        System.out.println("Загадайте число от 0 до 2_147_483_647.\n" + "Введите его, чтобы не забыть:");
+        strInput = scan.nextLine();
+
+        while (!strInput.equals(equal)) {
+            System.out.println("Это число больше, меньше или равно " + midRange + "? " + "Введите '>' или '<' или '=':");
+            strInput = scan.nextLine();
+            if (strInput.equals(more)) {
+                min = midRange;
+                midRange = (min + max) / 2;
+            }
+            if (strInput.equals(less)) {
+                max = midRange;
+                midRange = (min + max) / 2;
+            }
+            if (strInput.equals(equal)) {
+                System.out.println("Отлично! Ваше число " + midRange);
+            }
+        }
+    }
+}
+
+
+
